9d001a1f60a96d31f93fec4577747c355934a2fb
bagasteguhimani <bagasteguhimani10@gmail.com>
2021-10-02 23:30:33 +0700

    handler user

---
+++ backend/handlers/handler_user.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/radityaqb/tgtc/backend/dictionary"
+	"github.com/radityaqb/tgtc/backend/service"
+)
+
+func AddUser(w http.ResponseWriter, r *http.Request) {
+
+	var p dictionary.User
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		http.Error(w, "bad request", 400)
+		return
+	}
+
+	p = User.AddUser(context.Background(), p)
+	fmt.Fprintf(w, fmt.Sprint("success, id User : ", p.ID))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	idstring := r.URL.Query().Get("id")
+
+	idInt64, err := strconv.ParseInt(idstring, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p, err := User.GetUser(context.Background(), idInt64)
+	if err != nil {
+		// log.Fatal(err)
+		fmt.Fprintf(w, err.Error())
+	}
+
+	val, err := json.Marshal(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, string(val))
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	var p dictionary.User
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		http.Error(w, "bad request", 400)
+		return
+	}
+
+	User.DeleteUser(context.Background(), p.ID)
+	fmt.Fprintf(w, "success")
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var p dictionary.User
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		http.Error(w, "bad request", 400)
+		return
+	}
+
+	User.UpdateUser(context.Background(), p)
+
+	fmt.Fprintf(w, "success")
+}
+}
