f8c61c238e97a933db32018b32983b6113c552da
aralfayy <90131303+aralfayy@users.noreply.github.com>
2021-10-02 21:29:47 +0700

    sql jadi

---
+++ backend/domain/banner/banner.go
@@ -0,0 +1,12 @@
+// package main
+
+// import "fmt"
+// import "database/sql"
+// import _ "github.com/go-sql-driver/mysql"
+
+// type student struct {
+//     id    string
+//     name  string
+//     age   int
+//     grade int
+// }
